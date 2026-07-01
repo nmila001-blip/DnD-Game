38af6a2964b2d655288e314ea94e8631d5a0f1f5
nmila001-blip <nmila001@doral.colegia.org>
2026-07-01 08:41:26 -0400

    n

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include "Hero.h"
 #include "Enemy.h"
 #include "Game.h"
+#include "Weapon.h"
 using namespace std;
 int main() {
     srand(time(NULL));
+    Weapon weapon;
+    weapon.displayBag();
     Game game;
     game.Run();
 }
